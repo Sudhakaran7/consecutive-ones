ff4dc0af00c1fa715f146ef8012f20615a06bfc7
Sudhakaran_roni <32321500+Sudhakaran7@users.noreply.github.com>
2020-08-03 18:00:57 +0530

    Create consecutive-ones.py

---
+++ consecutive-ones.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def findIntegers(self, num):
+        dp = [1, 2]
+        for x in range(2, 32):
+            dp.append(dp[x - 1]+ dp[x - 2])
+        bnum = bin(num)[2:]
+        size = len(bnum)
+        ans = dp[size]
+        for idx in range(1, size):
+            if bnum[idx] == bnum[idx - 1] == '1':
+                break
+            if bnum[idx] == bnum[idx - 1] == '0':
+                ans -= dp[size - idx] - dp[size - idx - 1]
+        return ans
+val=Solution()
+n=int(input())
+print(val.findIntegers(n))
